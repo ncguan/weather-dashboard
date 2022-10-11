f8bd72e84c0ddd281b61baaf3bba4e87d9ce3773
NG <nicholeguan2@gmail.com>
2022-10-11 04:15:52 -0700

    display current weather information

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,12 +37,13 @@ var setCoord = function (info){
 getCoord('San Diego');
 
 var getCityInfo = function(y, x){
-    var apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + y + '&lon=' + x + '&appid=ec7a75b859f25b487d5d65395cbdeff9';
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + y + '&lon=' + x + '&appid=ec7a75b859f25b487d5d65395cbdeff9&units=imperial';
     fetch(apiUrl)
     .then(function (response) {
       if (response.ok) {
         response.json().then(function (data) {
           setWeather(data.list);
+          displayWeather();
         });
       } else {
         alert('Error: ' + response.statusText);
@@ -55,8 +56,40 @@ var getCityInfo = function(y, x){
 
 var setWeather = function(info){
     date = info[0].dt_txt;
-    icon = info[0].weather[0].icon;
+    var iconNum = info[0].weather[0].icon;
+    icon = "https://openweathermap.org/img/wn/" + iconNum + "@2x.png";
     temp = info[0].main.temp;
     humidity = info[0].main.humidity;
     wind = info[0].wind.speed;
+}
+
+var displayWeather = function (){
+    $("#currentWeather").append(
+        $(document.createElement('p')).attr({
+        class: 'heading',
+    })
+    );
+    $('.heading').text('San Diego'+date);
+    $(`<img src='${icon}'>`).appendTo('.heading');
+
+    $("#currentWeather").append(
+        $(document.createElement('p')).attr({
+        class: 'currentTemp',
+    })
+    );
+    $('.currentTemp').text('Temp: ' + temp + '°F');
+
+    $("#currentWeather").append(
+        $(document.createElement('p')).attr({
+        class: 'currentHumidity',
+    })
+    );
+    $('.currentHumidity').text('Humidity: ' + humidity + '%');
+
+    $("#currentWeather").append(
+        $(document.createElement('p')).attr({
+        class: 'currentWind',
+    })
+    );
+    $('.currentWind').text('Wind Speed: ' + wind + ' MPH');
 }
